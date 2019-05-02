e0887ab0649f851d4e7f723d25fe763aca51be91
nduhiu17 <nduhiu254@gmail.com>
2019-05-02 21:16:34 +0300

    create controller for updating a forum

--- src/main/java/com/example/postgresdemo/controller/ForumController.java
+++ src/main/java/com/example/postgresdemo/controller/ForumController.java
@@ -1,14 +1,13 @@
 package com.example.postgresdemo.controller;
 
+import com.example.postgresdemo.exception.ResourceNotFoundException;
 import com.example.postgresdemo.model.Forum;
+import com.example.postgresdemo.model.Question;
 import com.example.postgresdemo.repository.ForumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -27,5 +26,15 @@ public class ForumController {
     @PostMapping("/api/v1/forums")
     public Forum createForum(@Valid @RequestBody Forum forum) {
         return forumRepository.save(forum);
+    }
+
+    @PutMapping("/api/v1/forums/{forumId}")
+    public Forum updateForum(@PathVariable Long forumId,
+                                   @Valid @RequestBody Forum forumRequest) {
+        return forumRepository.findById(forumId)
+                .map(forum -> {
+                    forum.setTitle(forumRequest.getTitle());
+                    return forumRepository.save(forum);
+                }).orElseThrow(() -> new ResourceNotFoundException("Forum not found with id " + forumId));
     }
 }
